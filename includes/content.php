86baf87d97eea9aa07572dbe20c0fa576b5705ce
franb <franb.mail@gmail.com>
2018-04-24 18:32:38 -0300

    6to portfolio added

--- includes/content.php
+++ includes/content.php
@@ -474,21 +474,37 @@
                     </div>
                 </a>
             </div>
-            <!-- <div class="col-lg-4 col-sm-6">
+            <div class="col-lg-4 col-sm-6">
                 <a href="/img/portfolio/fullsize/6.jpg" class="portfolio-box">
                     <img src="/img/portfolio/thumbnails/6.jpg" class="img-responsive" alt="">
                     <div class="portfolio-box-caption">
-                        <div class="portfolio-box-caption-content">
+                        <div lang="en" class="portfolio-box-caption-content">
                             <div class="project-category text-faded">
-                                falta
+                                Solar panels
                             </div>
                             <div class="project-name">
-                                nombre
+                                Coming soon!
+                            </div>
+                        </div>
+                        <div lang="pt" class="portfolio-box-caption-content">
+                            <div class="project-category text-faded">
+                                Pain&eacute;is solares
+                            </div>
+                            <div class="project-name">
+                                ¡Logo!
+                            </div>
+                        </div>
+                        <div lang="es" class="portfolio-box-caption-content">
+                            <div class="project-category text-faded">
+                                Paneles solares
+                            </div>
+                            <div class="project-name">
+                                ¡Pronto!
                             </div>
                         </div>
                     </div>
                 </a>
-            </div> -->
+            </div>
         </div>
     </div>
 </section>
