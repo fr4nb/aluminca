cad618fd298a0070f5610b84d98aeaf48e6c7ad3
Francisco <franb.mail@gmail.com>
2018-10-27 19:18:16 -0300

    section completa

--- includes/content-promo.php
+++ includes/content-promo.php
@@ -1,1 +1,21 @@
 <?php require 'includes/nav.php'; ?>
+
+<main id="main">
+	
+	<section id="promo">
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-lg-12 text-center">
+	                <h2 class="section-heading">Promoci&oacute;n exclusiva.</h2>
+	                <hr class="primary">
+	            </div>
+
+	            <div class="col-lg-12 text-center">
+	            	<img src="img/matrices.jpg" class="img-fluid" alt="matrices para cortadora de fiambre">
+	                <a href="index.php#contact" class="btn btn-primary btn-xl page-scroll">Comunicate con nosotros</a>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+
+</main>
--- js/pages/promo.js
+++ js/pages/promo.js
@@ -2,9 +2,16 @@
     
  	"use strict";	
 
-    $('#mainNav').addClass('affix');
-    
+ 	var nav = $('#mainNav');
 
+    nav.addClass('affix').removeClass('affix-top');
+    
+	$(window).on("scroll", function() {
+	    var scrollPos = $(window).scrollTop();
+	    if (scrollPos <= 0) {
+	        nav.addClass('affix').removeClass('affix-top');
+	    }
+	});
 
     // init
     
